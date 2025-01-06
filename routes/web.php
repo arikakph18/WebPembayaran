9ca5921c2c923baf92d59bee4c0686800c88d90a
unknown <arikakarpina27@gmail.com>
2025-01-06 08:14:19 +0700

    menambahkan template mazer, & database

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.master');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/dashboard' ,[dashboardController::class, 'index']);
+Route::get('/login' ,[loginController::class, 'index'])->name('login');
+route::get('/register', [registerController::class, 'index']->name('login'));
+route::post('/regist', [registerController::class, 'store']->name('register.store'));
 
-Route::get('/home', function () {
-    return view('home');
-});
+
+
